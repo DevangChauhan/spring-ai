301ef9a9797833d8e9566d54d1a23c12c2ccfcb0
Devang Chauhan <devang.chauhan@benelog.com>
2024-05-07 09:23:28 +0530

    Revert "Revert "initial commit spring-ai project""
    This reverts commit e80115beb298d7483cac9579ff8595a5955bf6b5.

---
+++ src/test/java/com/learning/springai/services/OpenAIServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.learning.springai.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class OpenAIServiceImplTest {
+
+    @Autowired
+    OpenAIService openAIService;
+
+    @Test
+    void getAnswer() {
+        String answer = openAIService.getAnswer("Tell me a dad joke.");
+        System.out.println("Got the Answer");
+        System.out.println(answer);
+    }
+}
