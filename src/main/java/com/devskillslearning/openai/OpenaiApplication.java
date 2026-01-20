6feb7bd234c149e2917b9a2e28684c6d0e196ea4
Devang Chauhan <devang.chauhan@benelog.com>
2026-01-20 18:08:44 +0530

    implement multiple chat models in the application

--- src/main/java/com/devskillslearning/openai/OpenaiApplication.java
+++ src/main/java/com/devskillslearning/openai/OpenaiApplication.java
@@ -3,7 +3,7 @@ package com.devskillslearning.openai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.devskillslearning")
 public class OpenaiApplication {
 
     public static void main(String[] args) {
