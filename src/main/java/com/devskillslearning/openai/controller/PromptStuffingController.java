0bea33bbd027922bef15799a9ede961c82421ad2
Devang Chauhan <devang.chauhan@benelog.com>
2026-01-21 18:04:46 +0530

    implement prompt stuffing and prompt template scenarios

---
+++ src/main/java/com/devskillslearning/openai/controller/PromptStuffingController.java
@@ -0,0 +1,38 @@
+package com.devskillslearning.openai.controller;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PromptStuffingController {
+
+    private final ChatClient chatClient;
+
+    public PromptStuffingController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Value("classpath:/promptTemplates/systemPromptTemplate.st")
+    private Resource systemPromptTemplate;
+
+    @GetMapping("/prompt-stuffing")
+    public String promptStuffing(@RequestParam("message") String message) {
+        return chatClient
+                .prompt()
+                .system("""
+                        You are a professional customer service aasistant which helps drafting email responses 
+                        to improve the productivity of the customer support team.
+                        """)
+                .user(message)
+                .call()
+                .content();
+    }
+
+}
