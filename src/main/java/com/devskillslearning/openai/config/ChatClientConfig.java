4ccc922377f06bacda1aee24923deff9cc4192b0
Devang Chauhan <devang.chauhan@benelog.com>
2026-01-22 13:28:52 +0530

    configure chat options for chat client at default and specific contorller level

--- src/main/java/com/devskillslearning/openai/config/ChatClientConfig.java
+++ src/main/java/com/devskillslearning/openai/config/ChatClientConfig.java
@@ -4,6 +4,7 @@ package com.devskillslearning.openai.config;
 import com.devskillslearning.openai.advisors.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +15,15 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+
+        final ChatOptions chatOptions = ChatOptions.builder()
+                .model("gpt-4o-mini")
+                .temperature(0.8)
+                .build();
+
         return chatClientBuilder
                 .defaultAdvisors(List.of(new SimpleLoggerAdvisor(), new TokenUsageAuditAdvisor()))
+                .defaultOptions(chatOptions)
                 .defaultSystem(
                         """
                                 You are an internal IT helpdesk assistant. Your role is to assist\s
--- src/main/java/com/devskillslearning/openai/controller/PromptStuffingController.java
+++ src/main/java/com/devskillslearning/openai/controller/PromptStuffingController.java
@@ -2,6 +2,8 @@ package com.devskillslearning.openai.controller;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +28,7 @@ public class PromptStuffingController {
     public String promptStuffing(@RequestParam("message") String message) {
         return chatClient
                 .prompt()
+                .options(OpenAiChatOptions.builder().model(OpenAiApi.ChatModel.CHATGPT_4_O_LATEST).build())
                 .system("""
                         You are a professional customer service aasistant which helps drafting email responses 
                         to improve the productivity of the customer support team.
